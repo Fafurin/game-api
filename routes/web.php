b9909491bd6840bd3f1c2f59f773ceba93185ecf
Fafurin <ffafurin@mail.ru>
2022-07-09 13:06:35 +0300

    removed unnecessary routes

--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,41 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin'], function () {
+    Route::group(['namespace' => 'Main'], function () {
+        Route::get('/', 'IndexController')->name('admin.main.index');
+    });
+
+    Route::group(['namespace' => 'Game', 'prefix' => 'games'], function () {
+        Route::get('/', 'IndexController')->name('admin.game.index');
+        Route::get('/create', 'CreateController')->name('admin.game.create');
+        Route::post('/', 'StoreController')->name('admin.game.store');
+        Route::get('/{game}/edit', 'EditController')->name('admin.game.edit');
+        Route::patch('/{game}', 'UpdateController')->name('admin.game.update');
+        Route::delete('/{game}', 'DeleteController')->name('admin.game.delete');
+    });
+
+    Route::group(['namespace' => 'Studio', 'prefix' => 'studios'], function () {
+        Route::get('/', 'IndexController')->name('admin.studio.index');
+        Route::get('/create', 'CreateController')->name('admin.studio.create');
+        Route::post('/', 'StoreController')->name('admin.studio.store');
+        Route::get('/{studio}/edit', 'EditController')->name('admin.studio.edit');
+        Route::patch('/{studio}', 'UpdateController')->name('admin.studio.update');
+        Route::delete('/{studio}', 'DeleteController')->name('admin.studio.delete');
+    });
+
+    Route::group(['namespace' => 'Genre', 'prefix' => 'genres'], function () {
+        Route::get('/', 'IndexController')->name('admin.genre.index');
+        Route::get('/create', 'CreateController')->name('admin.genre.create');
+        Route::post('/', 'StoreController')->name('admin.genre.store');
+        Route::get('/{genre}/edit', 'EditController')->name('admin.genre.edit');
+        Route::patch('/{genre}', 'UpdateController')->name('admin.genre.update');
+        Route::delete('/{genre}', 'DeleteController')->name('admin.genre.delete');
+    });
 });
 
 Auth::routes();
