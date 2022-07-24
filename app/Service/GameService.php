c6905ba3d2f364d0603067fcb382da0f27588699
Fafurin <ffafurin@mail.ru>
2022-07-24 12:28:27 +0300

    made changes to the game service

--- app/Service/GameService.php
+++ app/Service/GameService.php
@@ -3,6 +3,8 @@
 namespace App\Service;
 
 use App\Models\Game;
+use App\Models\Genre;
+use App\Models\Studio;
 use Illuminate\Support\Facades\DB;
 
 class GameService
@@ -12,20 +14,38 @@ class GameService
     {
         try {
             Db::beginTransaction();
-            if(isset($data['genre_ids'])){
-                $genreIds = $data['genre_ids'];
-                unset($data['genre_ids']);
+
+            // создаем игру с названием name если ее еще нет в бд
+            $game = Game::firstOrCreate(['name' => $data['name']]);
+
+            // убираем лишние пробелы в жанрах
+            $genres = array_filter(array_map('trim', explode(',', $data['genres'])));
+
+            // получаем все жанры из бд
+            $dbGenres = Genre::get()->pluck('name');
+
+            // в цикле проверяем наличие жанра в бд, если его нет - создаем и присоединяем к модели игры
+            foreach ($genres as $genre) {
+                if (!($dbGenres->contains($genre))) {
+                    $newGenre = new Genre(['name' => $genre]);
+                    $game->genres()->attach($newGenre->id);
+                }
+                $genre = Genre::firstOrCreate(['name' => $genre]);
+                $game->genres()->attach($genre->id);
             }
 
-            $game = Game::firstOrCreate($data);
-            if(isset($genreIds)) {
-                $game->genres()->attach($genreIds);
-            }
+            // создаем студию если ее еще нет в бд
+            $studio = Studio::firstOrCreate(['name' => $data['studio']]);
+
+            // сохраняем игру в студию
+            $studio->games()->save($game);
+
             Db::commit();
-        } catch (\Exception $e){
+        } catch (\Exception $e) {
             Db::rollBack();
-            abort(500);
+            return response()->json(['error' => true, 'message' => 'Error'], 404);
         }
+        return $game;
     }
 
 //    метод для изменения игр в бд
@@ -33,19 +53,45 @@ class GameService
     {
         try {
             Db::beginTransaction();
-            if(isset($data['genre_ids'])){
-                $genreIds = $data['genre_ids'];
-                unset($data['genre_ids']);
+
+            // создаем игру с названием name если ее еще нет в бд
+            if (isset($data['name'])) {
+                $game->update(['name' => $data['name']]);
             }
 
-            $game->update($data);
-            if(isset($genreIds)){
-                $game->genres()->sync($genreIds);
+            if (isset($data['genres'])) {
+                // убираем лишние пробелы в жанрах
+                $genres = array_filter(array_map('trim', explode(',', $data['genres'])));
+
+                // получаем все жанры из бд
+                $dbGenres = Genre::get()->pluck('title');
+
+                // в цикле проверяем наличие жанра в бд, если его нет - создаем и присоединяем к модели игры
+                $genresArr = [];
+
+                foreach ($genres as $genre) {
+                    if (!($dbGenres->contains($genre))) {
+                        $newGenre = new Genre(['name' => $genre]);
+                        $genresArrp[] = $newGenre->id;
+                    }
+                    $genre = Genre::firstOrCreate(['name' => $genre]);
+                    $genresArr[] = $genre->id;
+                }
+                $game->genres()->sync($genresArr);
             }
+
+            if (isset($data['studio'])) {
+//          создаем студию если ее еще нет в бд
+                $studio = Studio::firstOrCreate(['name' => $data['studio']]);
+
+//          сохраняем игру в студию
+                $studio->games()->save($game);
+            }
+
             Db::commit();
-        } catch (\Exception $e){
+        } catch (\Exception $e) {
             Db::rollBack();
-            abort(500);
+            return response()->json(['error' => true, 'message' => 'Error'], 404);
         }
         return $game;
     }
