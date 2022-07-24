cfcc31620a92f5dfa5a91ce1bf118ef60a2cacd1
Fafurin <ffafurin@mail.ru>
2022-07-24 15:19:45 +0300

    removed requests

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -4,8 +4,12 @@ use App\Http\Controllers\Api\V1\GameController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/games', [GameController::class, 'index']);
-Route::get('/games/{id}', [GameController::class, 'show']);
-Route::get('/{genre}', [GameController::class, 'showByGenre']);
+Route::get('/games/{id}', [GameController::class, 'show'])
+    ->where('id', '[0-9]+');
+Route::get('/{genre}', [GameController::class, 'showByGenre'])
+    ->where('genre', '[A-Za-z]+');
 Route::post('/save', [GameController::class, 'save']);
-Route::put('/games/{id}', [GameController::class, 'update']);
-Route::delete('/games/{id}', [GameController::class, 'delete']);
+Route::put('/games/{id}', [GameController::class, 'update'])
+    ->where('id', '[0-9]+');
+Route::delete('/games/{id}', [GameController::class, 'delete'])
+    ->where('id', '[0-9]+');
