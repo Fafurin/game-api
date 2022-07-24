af918c9f374ae98576b6201e4747cdc4b8bdb870
Fafurin <ffafurin@mail.ru>
2022-07-24 12:25:51 +0300

    added the resources

---
+++ app/Http/Resources/GameResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class GameResource extends JsonResource
+{
+    public static $wrap = 'game';
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'studio' => $this->studio->name,
+            'genres' => $this->genres->pluck('name'),
+        ];
+    }
+}
