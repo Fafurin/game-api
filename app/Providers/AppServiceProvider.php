dcd8294010cb128a1bafdd77fd9cfc9eba6ddb89
Fafurin <ffafurin@mail.ru>
2022-08-02 12:22:50 +0300

    binded the contracts and the handlers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,8 +2,22 @@
 
 namespace App\Providers;
 
-use App\Handlers\Api\V1\GameHandlerContract;
+use App\Commands\Api\V1\CreateGameCommandHandler;
+use App\Commands\Api\V1\CreateGameCommandHandlerContract;
+use App\Commands\Api\V1\UpdateGameCommandHandler;
+use App\Commands\Api\V1\UpdateGameCommandHandlerContract;
+use App\Handlers\Api\V1\GameDeleteHandlerContract;
+use App\Handlers\Api\V1\GameDeleteHandler;
+use App\Handlers\Api\V1\GameListHandlerContract;
 use App\Handlers\Api\V1\GameListHandler;
+use App\Handlers\Api\V1\GameStoreHandlerContract;
+use App\Handlers\Api\V1\GameStoreHandler;
+use App\Handlers\Api\V1\GameUpdateHandler;
+use App\Handlers\Api\V1\GameUpdateHandlerContract;
+use App\Handlers\Api\V1\GameViewByGenreHandler;
+use App\Handlers\Api\V1\GameViewByGenreHandlerContract;
+use App\Handlers\Api\V1\GameViewHandler;
+use App\Handlers\Api\V1\GameViewHandlerContract;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -15,7 +29,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind(GameHandlerContract::class, GameListHandler::class);
+        $this->app->bind(GameListHandlerContract::class, GameListHandler::class);
+        $this->app->bind(GameStoreHandlerContract::class, GameStoreHandler::class);
+        $this->app->bind(GameDeleteHandlerContract::class, GameDeleteHandler::class);
+        $this->app->bind(GameUpdateHandlerContract::class, GameUpdateHandler::class);
+        $this->app->bind(GameViewHandlerContract::class, GameViewHandler::class);
+        $this->app->bind(GameViewByGenreHandlerContract::class, GameViewByGenreHandler::class);
+        $this->app->bind(CreateGameCommandHandlerContract::class, CreateGameCommandHandler::class);
+        $this->app->bind(UpdateGameCommandHandlerContract::class, UpdateGameCommandHandler::class);
     }
 
     /**
