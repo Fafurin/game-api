f63eaf7eb76b8407299812c59653fa39e003c3b3
Fafurin <ffafurin@mail.ru>
2022-08-02 12:24:57 +0300

    rewrote the routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -19,6 +19,8 @@ class RouteServiceProvider extends ServiceProvider
      */
     public const HOME = '/home';
 
+    protected $namespace = 'App\\Http\\Controllers\\Api';
+
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
      *
@@ -31,6 +33,7 @@ class RouteServiceProvider extends ServiceProvider
         $this->routes(function () {
             Route::middleware('api')
                 ->prefix('api/v1')
+                ->namespace($this->namespace)
                 ->group(base_path('routes/api_v1.php'));
 
             Route::middleware('web')
--- routes/api_v1.php
+++ routes/api_v1.php
@@ -5,27 +5,27 @@ use Illuminate\Support\Facades\Route;
 // пропускаем маршруты через посредников:
 // auth_api - простая аутентификация по токену;
 // api_json - возвращает ответы в json-формате
-Route::group(['middleware' => ['api_json'], 'match' => ['post', 'get']], function () {
+Route::group(['middleware' => ['api_json'], 'namespace' => 'V1', 'match' => ['post', 'get']], function () {
 
     // маршрут для вывода всех игр из бд
-    Route::get('/games', [\App\Http\Controllers\Api\V1\GameListController::class, 'index']);
+    Route::get('/games', 'GameListController');
 
     // маршрут для вывода игры по id
-    Route::get('/games/{id}', [\App\Http\Controllers\Api\V1\GameViewController::class, 'index'])
+    Route::get('/games/{id}', 'GameViewController')
         ->where('id', '[0-9]+');
 
     // маршрут для вывода игр по названию жанра
-    Route::get('/{genre}', [\App\Http\Controllers\Api\V1\GameViewByGenreController::class, 'index'])
+    Route::get('/{genre}', 'GameViewByGenreController')
         ->where('genre', '[A-Za-z0-9]+');
 
     // маршрут для создания игры
-    Route::post('/save', [\App\Http\Controllers\Api\V1\GameStoreController::class, 'index']);
+    Route::post('/save', 'GameStoreController');
 //
     // маршрут для изменения игры
-    Route::put('/games/{id}', [\App\Http\Controllers\Api\V1\GameUpdateController::class, 'index'])
+    Route::put('/games/{id}', 'GameUpdateController')
         ->where('id', '[0-9]+');
 //
     // маршрут для удаления игры
-    Route::delete('/games/{id}', [\App\Http\Controllers\Api\V1\GameDeleteController::class, 'index'])
+    Route::delete('/games/{id}', 'GameDeleteController')
         ->where('id', '[0-9]+');
 });
